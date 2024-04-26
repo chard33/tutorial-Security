9d1b4dd1c4667f462d349974a9944d57fd83e28c
Chard33 <jchar33@hotmail.com>
2024-04-25 23:22:47 -0600

    Primer commit

---
+++ src/main/java/com/tutorialSecurity/tutorialSecurity/TutorialSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.tutorialSecurity.tutorialSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorialSecurity/tutorialSecurity/TutorialSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorialSecurity.tutorialSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
